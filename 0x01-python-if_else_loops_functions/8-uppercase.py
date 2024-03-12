632a56faec0debba2a5b15d4140ede7f07d1e14f
hopemumbi <mumbimak98@gmail.com>
2024-03-12 14:16:55 +0000

    update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
+    # iterate over each character in str
     for i in str:
+        # check if the character is lowercase
         if (ord(i) >= 97 and ord(i) <= 122):
+            # convert the lowercase to uppercase
             i = chr(ord(i) - 32)
+        # print the charaster either coverted or otherwise
         print('{}'.format(i), end="")
+    # print a newline at the end
     print()
