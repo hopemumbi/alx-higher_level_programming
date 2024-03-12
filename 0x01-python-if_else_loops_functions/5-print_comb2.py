ffc874b3201849d1387728afdbbb53675b25081d
hopemumbi <mumbimak98@gmail.com>
2024-03-12 11:54:35 +0000

    update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,4 +4,4 @@ for number in range(0, 99):
     # print separated by comma in ascending order, with two digits
     print('{:02d}'.format(number), end=", ")
 # omit comma at the end
-print('{:02d}'.format(99))
+print('99')
