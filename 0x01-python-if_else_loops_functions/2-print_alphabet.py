86cf71828c00d5ef9436e3c79f03a8e4fdcbac65
hopemumbi <mumbimak98@gmail.com>
2024-03-12 10:32:29 +0000

    update  2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-#Use for to loop through the lowercase ASCII range
-for alphabet in range(97, 123):
-    #print the ASCII alphabet, not followed by a new line.
-    print('{:c}'.format(alphabet), end="")
+# Use only one loop
+for c in range(123):
+    if (c >= 97) and (c <= 122):
+        # print the ASCII alphabet, not followed by a new line.
+        print('{:c}'.format(c), end="")
