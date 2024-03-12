ab8d2fb8423a0aa5cc8d7e30719e354e859984b5
hopemumbi <144779094+hopemumbi@users.noreply.github.com>
2024-03-12 15:09:10 +0300

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,4 @@
 # use one loop to print the ASCII alphabet
 for alpha in range(97, 123):
     # print, not followed by a new line
-    print("{}".format(chr(alpha)), end="")
+    print("{:c}".format(alpha), end="")
