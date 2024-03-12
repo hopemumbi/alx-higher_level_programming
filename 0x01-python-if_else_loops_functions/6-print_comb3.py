0d1f3fd37615f1043d05b7dac12c2a413408b719
hopemumbi <mumbimak98@gmail.com>
2024-03-12 12:52:20 +0000

    update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-#prints all possible different combinations of two digits
-for i in range(10):
-    for j in range(10):
-        print('{}{}'.format(i, j))
+# prints all possible different combinations of two digits
+for i in range(0, 10):
+    # initialilize witn i so 01 and 10 are the same combination
+    for j in range(i, 10):
+        # the two digits must be different
+        if (i != j):
+            # print in ascending order
+            print('{}{}'.format(str(i), str(j)), end=", ")
+            # last number should be followed by a new line
+            if (i + j == 17):
+                print('{}{}'.format(str(i), str(j)))
