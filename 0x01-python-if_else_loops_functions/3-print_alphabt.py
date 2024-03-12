2eec4b87c8a26b868818bfce0a727ef1b8df1c63
hopemumbi <mumbimak98@gmail.com>
2024-03-12 10:42:54 +0000

    update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 # use for loop through the alphabet
-for c in range(123):
+for c in range(97, 123):
     # Use if statement to omit 'q' and 'e'
-    if (c >= 97) and (c <= 123):
-        if (c != 101) and (c != 113):
-            # print ASCII alphabet not followed by a new line.
-            print('{:c}'.format(c), end="")
+    if (c != 101) and (c != 113):
+        # print ASCII alphabet not followed by a new line.
+        print('{:c}'.format(c), end="")
