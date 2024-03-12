0bf1c75c51e7d6837ef7ab60303c7803ece1bbe5
hopemumbi <mumbimak98@gmail.com>
2024-03-12 15:01:46 +0000

    update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,7 +6,7 @@ def fizzbuzz():
         if i % 3 == 0 and i % 5 == 0:
             print('FizzBuzz', end=" ")
         # multiples of three print Fizz
-        if i % 3 == 0:
+        elif i % 3 == 0:
             print('Fizz', end=" ")
         # multiples of five print Buzz
         elif i % 5 == 0:
