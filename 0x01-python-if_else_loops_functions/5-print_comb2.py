16e471084d3614699236b72b0110d593ed545a64
hopemumbi <mumbimak98@gmail.com>
2024-03-12 12:04:13 +0000

    update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# use for to loop from 0 to 99
+# use only one loop from 0 to 99
 for number in range(0, 100):
-    # omit comma at the end
-    if number == 99:
-        print('{}'.format(number))
     # print separated by comma in ascending order, with two digits
-    else:
+    if number < 99:
         print('{:02d}'.format(number), end=", ")
+    # omit comma at the end
+    else:
+        print('{}'.format(number))
