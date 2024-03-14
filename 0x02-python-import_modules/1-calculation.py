3bddac1c93ac6d19c5699248fe59b091c89d5160
hopemumbi <mumbimak98@gmail.com>
2024-03-14 16:41:11 +0000

    update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# import calculator_1 module
-from calculator_1 import add, sub, mul, div
-# set variables a and b
-a = 10
-b = 5
 # ensure code is not executed when imported
 if __name__ == "__main__":
+    # import calculator_1 module
+    from calculator_1 import add, sub, mul, div
+    # set variables a and b
+    a = 10
+    b = 5
     # print addition
     print(a, "+", b, "=", add(a, b))
     # print subtraction
