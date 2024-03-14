0497d53e47ebfb783290ce8628ebb8759679a0bf
hopemumbi <mumbimak98@gmail.com>
2024-03-14 17:40:03 +0000

    update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    # print addition
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    # print subtraction
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    # print multiplication
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    # print division
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    # output addition
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    # output subtraction
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    # output multiplication
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    # outpu division
+    print("{} / {} = {}".format(a, b, div(a, b)))
