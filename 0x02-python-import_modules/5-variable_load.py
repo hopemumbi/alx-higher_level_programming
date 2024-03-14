98eeb3b502e5ac9f56e335ad3ea3e102d7b603e6
hopemumbi <mumbimak98@gmail.com>
2024-03-14 17:36:04 +0000

    update 5-variable_load.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# check if the script is being run as the main program
+if __name__ == "__main__":
+    # Import the variable "a" from variable_load_5
+    from variable_load_5 import a
+    # print the value of a
+    print(a)
