1c1a025d2ad8d74b642b9b8e6ec80c485fba8c27
hopemumbi <mumbimak98@gmail.com>
2024-03-14 17:47:25 +0000

    update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-
-# ensure code is not executed when imported
 if __name__ == "__main__":
-    # import calculator_1 module
     from calculator_1 import add, sub, mul, div
-
-    # set variables a and b
     a = 10
     b = 5
-
-    # output addition
-    print(a, "+", b, "=", add(a, b))
-    # output subtraction
-    print(a, "-", b, "=", sub(a, b))
-    # output multiplication
-    print(a, "*", b, "=", mul(a, b))
-    # output division
-    print(a, "/", b, "=", div(a, b))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
