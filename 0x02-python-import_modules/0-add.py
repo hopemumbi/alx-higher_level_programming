ccc495dd5d5be21fbd28885cf161903d46de3d93
hopemumbi <mumbimak98@gmail.com>
2024-03-14 15:42:53 +0000

    update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+
+# import the add function from add_.py
 from add_0 import add
+
+# define variables a and b
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    # print the result
+    print("{} + {} = {}".format(a, b, add(a, b)))
