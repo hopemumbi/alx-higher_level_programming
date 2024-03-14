a72e3df6b754b8570e3303b593e116de56bf89cd
hopemumbi <mumbimak98@gmail.com>
2024-03-14 16:38:51 +0000

    update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-# import sys module
-from sys import argv
-# variable argc = number of args
-argc = len(argv) - 1
-# if number of args == 1 then
-if argc == 1:
-    # print Number of argument(s) followed by argument
-    print(argc, "argument:")
-# else if argc == 0
-elif argc == 0:
-    print(argc, "arguments.")
-# else
-else:
-    # print Number of argument(s) followed by arguments
-    print(argc, "arguments:")
-# initialize a variable i to 1;
-i = 1
-# while  i <= argc then
-while i <= argc:
-    # print the <argument vector>  :, <argument value> and a new line
-    print(i, ":", argv[i])
-    # increment i to move to the next argument
-    i += 1
+if __name__ == "__main__":
+    # import sys module
+    from sys import argv
+
+    # variable argc = number of args
+    argc = len(argv) - 1
+    # if number of args == 1 then
+    if argc == 1:
+        # print Number of argument(s) followed by argument
+        print(argc, "argument:")
+    # else if argc == 0
+    elif argc == 0:
+        print(argc, "arguments.")
+    # else
+    else:
+        # print Number of argument(s) followed by arguments
+        print(argc, "arguments:")
+    # initialize a variable i to 1;
+    i = 1
+    # while  i <= argc then
+    while i <= argc:
+        # print the <argument vector>  :, <argument value> and a new line
+        print(i, ":", argv[i])
+        # increment i to move to the next argument
+        i += 1
