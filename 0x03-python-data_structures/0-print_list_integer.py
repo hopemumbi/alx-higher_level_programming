aaa4aeb8e091ad4a22f130df97522b35ee4cf072
hopemumbi <mumbimak98@gmail.com>
2024-03-15 11:46:57 +0000

    update 0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """function that prints all integers of a list.
+
+    Args:
+        my_list=[] - the list of integers
+
+    """
+    # use str.format() to print integers
+    print("{}".format(my_list))
