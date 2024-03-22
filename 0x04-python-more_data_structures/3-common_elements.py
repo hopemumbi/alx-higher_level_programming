dd614febbb8db8a54a7db6eefb8811d6ed96df8d
hopemumbi <mumbimak98@gmail.com>
2024-03-22 08:33:51 +0300

    update 3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """function that returns a set of common elements in two sets.
+
+
+    Args:
+        set_1: first set
+        set_2: second set
+
+    Return:
+        common elements
+    """
+    # elements in both set_a and set_b
+    return set_1 & set_2
