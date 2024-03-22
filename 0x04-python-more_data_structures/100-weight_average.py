1254a2abad6b66f31dbb9318d9da3f83a5222fc5
hopemumbi <mumbimak98@gmail.com>
2024-03-22 11:51:37 +0300

    update 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    # create an empty list
+    new_list = []
+    # iterate through the list then
+    for tup in my_list:
+        # convert each tuple to a list and add it to new_list
+        new_list.append(list(tup))
+
+    # calculate the numerator
+    numerator = sum([score * weight for score, weight in new_list])
+    # calculte the denominator
+    denominator = sum([weight for _, weight in new_list])
+    # return the weighted average
+    weighted_average = numerator / denominator
+    return weighted_average
