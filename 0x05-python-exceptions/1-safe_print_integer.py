becd13ada1aa8a7193f42528375ea54ac2b7a372
hopemumbi <mumbimak98@gmail.com>
2024-04-02 16:21:35 +0300

    update 1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        # Print integer followed by a new line
+        print("{:d}".format(value))
+        # Return True if value is an integer
+        return True
+    except:
+        # Return False if value is not an integer
+        return False
