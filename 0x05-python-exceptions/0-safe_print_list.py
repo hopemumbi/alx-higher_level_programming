090ba709de99fc32bc0f628a9e2a77bd500f26c9
hopemumbi <mumbimak98@gmail.com>
2024-04-02 17:07:50 +0300

    update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
+        # Iterate through my_list up to x
         for i in range(x):
+            # print only integers on the same line
             print(my_list[i], end="")
-    except IndexError:
-        x = i
-    finally:
+            count += 1
+    except (IndexError, TypeError):
+        pass
+    else:
+        # print a new line
         print()
-        return x
+    # return the number of elements printed
+    return count
