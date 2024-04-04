8ad87e924845760d00d1bd477fc14f8857388db5
hopemumbi <mumbimak98@gmail.com>
2024-04-04 16:25:43 +0300

    update 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializes a square instance
+
+        Args:
+            size (int): The size of the square
+        """
+        # Raise a Type Error if size is not an integer
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        # Raise a ValueError if size < 0
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size      # __ makes size a private instance
