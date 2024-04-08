752ac685c79d73db9051b0441aaabb106c5b4d90
hopemumbi <mumbimak98@gmail.com>
2024-04-08 05:53:43 +0300

    update 3-say_my_name.py, tests/3-say_my_name.txt

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for printing name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints  My name is <first name> <last name>
+    
+    Args:
+        first_name (str): the first name
+        last_name (str): the last name
+
+    Raises:
+        TypeError: if arguments are not strings
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
