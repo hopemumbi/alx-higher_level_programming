fd89be43562fddbf9e4c412bff4f136bfbe4f11d
hopemumbi <mumbimak98@gmail.com>
2024-04-08 07:36:12 +0300

    update 4-print_square.py, tests/4-print_square.txt

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for printing a square"""
+def print_square(size):
+    """ function that prints a square with the character #.
+
+    Args:
+        size (int): The height/width of the square.
+
+    Raises:
+        TypeError: If `size` is not an integer
+        ValueError: Is size < 0
+
+    """
+    # TypeError: If `size` is not an integer
+    if not isinstance(size, int) or isinstance(size, bool):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    # Iterate through range(size) to print each row
+    for _ in range(size):
+        # Print a row of # characters of length `size`
+        print("#" * size)
