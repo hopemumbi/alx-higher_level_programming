6589381a155dbdd55e4f6a8aa1b4c07bb6dd1881
hopemumbi <mumbimak98@gmail.com>
2024-04-10 09:12:56 +0300

    update  4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Module that defines a class Square"""
+
+
+class Square:
+    """Template that represents a square"""
+
+    def __init__(self, size=0):
+        """Initializes a square instance
+
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size      # __ makes size a private instance
+
+    @property
+    def size(self):
+        """Get the size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the value of size"""
+
+        # Raise a Type Error if size is not an integer
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        # Raise a ValueError if size < 0
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value      # __ makes size a private instance
+
+    def area(self):
+        """"returns the current square area
+
+        Return:
+            the area
+        """
+        return self.__size**2
