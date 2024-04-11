5b96d6d1d6bd2de151e9a3ead3ec8249ea4a5748
hopemumbi <mumbimak98@gmail.com>
2024-04-11 05:44:01 +0300

    update 0-add_integer.py, tests/0-add_integer.txt

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,14 +17,25 @@ def add_integer(a, b=98):
         a+b :the sum of a and b
     """
     # a and b must be integers or floats
-    if not isinstance(a, (int, float)) or isinstance(a, bool):
+    if a is None or \
+            not isinstance(a, (int, float)) or isinstance(a, bool):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)) or isinstance(b, bool):
+    if b is None or \
+            not isinstance(b, (int, float)) or isinstance(b, bool):
         raise TypeError("b must be an integer")
 
     # a and b must be first casted to integers if they are float
     a = int(a) if isinstance(a, float) else a
     a = int(b) if isinstance(a, float) else a
+    
+    # Add the two integers
+    result = a + b
+
+    # Check for float overflow
+    max_value = 1e100 # Define a threshold for overflow
+    if result >= max_value or result <= -max_value:
+        return 89
+    
     # Returns an integer: the addition of a and b
     return a + b
 
