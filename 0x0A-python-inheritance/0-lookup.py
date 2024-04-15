3ef73826f37b0619f0b3d944f55e86ae6d24c5f3
hopemumbi <mumbimak98@gmail.com>
2024-04-15 14:11:15 +0300

    update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 def lookup(obj):
-    """ function that returns the list of
-    available attributes and methods of an object:
-    Args:
-        obj: The instance to be listed
     """
-    # Create an empty list to store the attributes and methods
-    lst = []
+    Return a list of available attributes and methods of an object.
 
-    # Use a dictionary to access the key for each attributes and method
-    for name in obj.__dict__.keys():
-        # Append the key to the list
-        lst.append(name)
-    # Return the list of attributes and methods
-    return lst
+    Args:
+        obj: An object to inspect.
+
+    Returns:
+        A list containing the names of attributes and methods of the object.
+    """
+    return dir(obj)
