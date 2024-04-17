686df3eeeb493244e95c7e0ad2d85c1d8325ecce
hopemumbi <mumbimak98@gmail.com>
2024-04-17 17:44:36 +0300

    update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -25,7 +25,8 @@ class Student:
             dict: A dictionary of attributes of the Student instance.
         """
         # If attrs is a list and all elements in attrs are strings
-        if (type(attrs) == list and all(type(key) == str for key in attrs)):
+        if (type(attrs) == list and
+                all(type(key) == str for key in attrs)):
             # Create a dictionary using comprehension
             # hasattr - Check if the key exists in the list
             # getattr - get the value of the key
