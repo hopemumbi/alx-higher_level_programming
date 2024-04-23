6421b74517db7ddfbff1e3aec6f4e1f360b268dc
hopemumbi <mumbimak98@gmail.com>
2024-04-23 10:11:05 +0300

    update tests/test_base.py

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,5 +1,6 @@
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class TestBaseClass(unittest.TestCase):
@@ -40,6 +41,31 @@ class TestBaseClass(unittest.TestCase):
         b5 = Base()
         # Asserting that the id of b5 is 4
         self.assertEqual(b3.id, b5.id - 1)
+
+
+class TestBase_to_json_string(unittest.TestCase):
+    """Test cases for method to_json_string() of class base"""
+
+    def test_to_json_string_with_empty_list(self):
+        json_string = Base.to_json_string([])
+        self.assertEqual(json_string, "[]")
+
+    def test_to_json_string_with_none(self):
+        json_string = Base.to_json_string(None)
+        self.assertEqual(json_string, "[]")
+
+    def test_to_json_dictionary_type(self):
+        r1 = Rectangle(10, 7, 2, 6)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(type(json_dictionary), str)
+
+    def test_to_json_string_with_list_of_dictionaries(self):
+        r1 = Rectangle(10, 7, 2, 8, 6)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        expected = '[{"id": 6, "width": 10, "height": 7, "x": 2, "y": 8}]'
+        self.assertEqual(json_dictionary, expected)
 
 
 if __name__ == '__main__':
